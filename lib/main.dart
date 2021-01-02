7261c0a543dd75183c7941f6acdaa407a0e4cd3f
priyadarshu <priyadarshinichettiar@gmail.com>
2021-01-02 23:43:46 +0530

    Added the images to pubspec.yaml file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:page_view_example/pages/pages.dart';
 
 void main() {
   runApp(MyApp());
--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -1,12 +1,24 @@
-
 import 'package:page_view_example/pages/pages_entries.dart';
 
-final List<PageEntry> pages=[
+final List<PageEntry> pages = [
   PageEntry(
     title: 'The Elephants',
-    image: '',
-    description: 'Elephants are amazing animals. Baby elephants are more adorable.'
-    'Elephants are mammals of the family Elephantidae and the largest existing land animals.'
-    'Three species are currently recognised: the African bush elephant, the African forest elephant, and the Asian elephant.'
+    image: 'assets/images/elephants.jpeg',
+    description:
+        'Elephants are amazing animals. Baby elephants are more adorable.'
+        'Elephants are mammals of the family Elephantidae and the largest existing land animals.'
+        'Three species are currently recognised: the African bush elephant, the African forest elephant, and the Asian elephant.',
   ),
-]
+  PageEntry(
+      title: 'The Horses',
+      image: 'assets/images/horses.jpeg',
+      description: 'The horse is one of two extant subspecies of Equus ferus.'
+          'It is an odd-toed ungulate mammal belonging to the taxonomic family Equidae.'
+          'The horse has evolved over the past 45 to 55 million years from a small multi-toed creature, Eohippus, into the large, single-toed animal of today.'),
+  PageEntry(
+      title: 'The Fishes',
+      image: 'assets/images/fishes.jpeg',
+      description:
+          'Fish, any of approximately 34,000 species of vertebrate animals (phylum Chordata) found in the fresh and salt waters of the world.'
+          'Living species range from the primitive jawless lampreys and hagfishes through the cartilaginous sharks, skates, and rays to the abundant and diverse bony fishes.'),
+];
